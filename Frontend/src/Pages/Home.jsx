f17ae05b949ef925b99a5f8115774f0fd62f6bc6
faisal-din <dahrifaisal56@gmail.com>
2025-05-28 21:03:38 +0500

    created wxclusive offers component

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Hero from '../Components/Hero';
 import FeaturedDestinations from '../Components/FeaturedDestinations';
+import ExclusiveOffers from '../Components/ExclusiveOffers';
 
 const Home = () => {
   return (
     <>
       <Hero />
       <FeaturedDestinations />
+      <ExclusiveOffers />
     </>
   );
 };
