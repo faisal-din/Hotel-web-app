df49bf072adf57a7fa1e8df1b71f8067b9d958df
faisal-din <dahrifaisal56@gmail.com>
2025-05-28 20:38:09 +0500

    created featured destinations component

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from '../Components/Hero';
+import FeaturedDestinations from '../Components/FeaturedDestinations';
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <FeaturedDestinations />
     </>
   );
 };
