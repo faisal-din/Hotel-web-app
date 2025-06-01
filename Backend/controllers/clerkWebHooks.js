b73cbcc693e33ea4d570a8478068760d49c55fa7
faisal-din <dahrifaisal56@gmail.com>
2025-06-01 22:57:30 +0500

    fixed webhooks headers verify error

--- Backend/controllers/clerkWebHooks.js
+++ Backend/controllers/clerkWebHooks.js
@@ -7,22 +7,24 @@ export const clerkWebHooks = async (req, res) => {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
     // Getting headers
-    const header = {
+    const headers = {
       'svix-id': req.headers['svix-id'],
       'svix-timestamp': req.headers['svix-timestamp'],
       'svix-signature': req.headers['svix-signature'],
     };
 
     // Verify the webhook signature
-    await whook.verify(JSON.stringify(req.body, header));
+    await whook.verify(JSON.stringify(req.body), headers);
 
     // Getting Data from the request body
-
     const { data, type } = req.body;
+
+    console.log('Event type:', type);
+    console.log('User data:', data);
 
     const userData = {
       _id: data.id,
-      email: data.email_addresses[0]?.email_address,
+      email: data.email_addresses[0].email_address,
       username: data.first_name + ' ' + data.last_name,
       image: data.image_url,
     };
@@ -51,7 +53,7 @@ export const clerkWebHooks = async (req, res) => {
       message: 'Webhook Received',
     });
   } catch (error) {
-    console.error('Error handling Clerk webhook:', error);
+    console.error('Error handling Clerk webhook:', error.message);
     return res.status(500).json({
       success: false,
       error: error.message,
